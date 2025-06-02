779b6041c45d11c076075a97d942c30370dfd9a8
tommyiversonj <tommyiversonj@gmail.com>
2025-06-02 07:35:26 -0700

    Create a sentence

---
+++ 4-concat.js
@@ -0,0 +1,6 @@
+// Get the first and second arguments passed by the user
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// Print the sentence in the required format: "<firstArg> is <secondArg>"
+console.log(`${firstArg} is ${secondArg}`);
